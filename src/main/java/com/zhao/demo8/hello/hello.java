a75f57a73228330fc4c4027c24b070f0639318f0
Augailife <1098159607@qq.com>
2020-09-20 20:37:47 +0800

    the firstday end

--- src/main/java/com/zhao/demo8/hello/hello.java
+++ src/main/java/com/zhao/demo8/hello/hello.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller//允许这个类接收前端的请求
 public class hello{
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
 
     }
 }
